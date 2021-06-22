d4233fd674eb8bd200a44bbf495e8f31d3d531da
sharthak123 <sayanmallik1995@gmail.com>
2021-06-22 21:12:49 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vaccinator.Controllers
+{
+    public class HomeController : Controller
+    {
+        /**
+         *  returns landing page of the application
+         */
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        /**
+         *  returns an about page 
+         */
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+
+        /**
+         *  returns a contact details page
+         */
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("Contact");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vaccinator.MvcApplication" Language="C#" %>
